e28f32139b6e7eb8424a781e877fdf7a390a7c60
Deborah Kurz <DeborahKurz6@gmail.com>
2024-01-13 11:13:48 -0700

    Complete Lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
+//>>>selectedCategory variable will keep track of the select tag value
+
+//use selectedCategory to determine which items to display in the sopping list.
+  //use .filter()to filter based on the selected Category.
+
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  function handleChange(event){
+    setSelectedCategory(event.target.value);
+  };
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,9 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {items
+          .filter(item => selectedCategory === "" || item.category === selectedCategory);
+          .map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
